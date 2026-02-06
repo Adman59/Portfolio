229c561616d77d6cd0de25416cadfca5983242f7
Adrien Magnies <adrien.magnies@hotmail.fr>
2026-02-06 11:28:21 +0100

    scss configure to start project with variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
-import Image from 'next/image';
-import styles from './page.module.css';
 import Hero from '@/components/Hero/Hero';
 import Realisation from '@/components/Realisation/Realisation';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div className={styles.intro}>
-          <section style={{ height: '100vh' }} />
-          <Hero />
-          <Realisation />
-          <section style={{ height: '200vh' }} />
-        </div>
-      </main>
-    </div>
+    <main>
+      <div>
+        <section style={{ height: '100vh' }} />
+        <Hero />
+        <section style={{ height: '200vh' }} />
+      </div>
+    </main>
   );
 }
